83489b7d089a7b6cbe0c90f5801c48f5641b8755
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-07-17 21:42:37 +0700

    Add test case

--- src/server/test.ts
+++ src/server/test.ts
@@ -1,5 +1,22 @@
 import test from "ava";
 
-test("Foo", (t) => {
-    t.pass();
+import { IGame } from "../package/interface/IGame";
+import { GameImpl } from "../package/implementation/GameImpl";
+import { RoomImpl } from "../package/implementation/RoomImpl";
+
+let game: IGame;
+
+test.before((t) => {
+    game = new GameImpl();
+    const room = game.createRoom();
+
+    t.notDeepEqual(room, undefined);
+    t.notDeepEqual(room, null);
+});
+
+test("Get Room", (t) => {
+    const room = game.getRoom(1);
+    const expectedRoom = new RoomImpl(1);
+
+    t.deepEqual(room, expectedRoom, "Room isn't same!");
 });
