68a813b6e71058b1e0c92fcc8a89578d83d0b722
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-07-17 10:19:17 +0700

    Set up main app

---
+++ src/server/app.ts
@@ -0,0 +1,21 @@
+import bodyParser, { urlencoded } from "body-parser";
+import express from "express";
+import { registerRoute } from "../package/routes/list";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+registerRoute(app);
+
+app.listen(port, (error) => {
+    if (error) {
+        return console.error(error);
+    }
+
+    return console.log("Server is listening at port 3000");
+});
