51787a2a3b5ab52ba303de1174120b317dba4e0e
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-07-17 10:19:55 +0700

    Make Position utility for tic-tac-toe game

---
+++ src/package/utilities/Position.ts
@@ -0,0 +1,9 @@
+export class Position {
+    public row: number;
+    public column: number;
+
+    constructor(row: number, column: number) {
+        this.row = row;
+        this.column = column;
+    }
+}
