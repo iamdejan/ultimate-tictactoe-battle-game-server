2b024661cd5efcf4afd4d9975d5b1bd0c9ea5617
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-07-17 18:33:26 +0700

    Create route for create room

--- src/package/routes/functions.ts
+++ src/package/routes/functions.ts
@@ -1,8 +1,18 @@
 import { Express, Request, Response } from "express";
 import { IGame } from "../interface/IGame";
 import { IRoom } from "../interface/IRoom";
+import { GeneralResponse } from "../utilities/GeneralResponse";
 
 export async function createRoom(request: Request, response: Response, game: IGame) {
-    const room: IRoom = game.createRoom();
-    response.json(JSON.stringify(room));
+    const result: GeneralResponse = new GeneralResponse();
+    try {
+        const room: IRoom = game.createRoom();
+        result.result = room;
+        result.success = true;
+    } catch (error) {
+        result.result = {};
+        result.success = false;
+        result.message = error.message;
+    }
+    response.json(result);
 }
