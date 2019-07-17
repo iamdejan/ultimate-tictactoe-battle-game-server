7b91f5627bb09928fbcfd251043ca855de9e13cd
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-07-17 16:54:27 +0700

    Refactor routes

--- src/package/routes/list.ts
+++ src/package/routes/list.ts
@@ -1,7 +1,16 @@
 import { Express } from "express";
 import * as func from "./functions";
 
+import { GameImpl } from "../implementation/GameImpl";
+import { IGame } from "../interface/IGame";
+
 export function registerRoute(app: Express) {
+    registerDebugRoute(app);
+
+    const game: IGame = new GameImpl();
+}
+
+function registerDebugRoute(app: Express) {
     app.get("/", (request, response) => {
         func.helloWorld(request, response);
     });
