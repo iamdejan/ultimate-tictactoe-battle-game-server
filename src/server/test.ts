cf3b58af25393ba5e1be625d457fbc330a28b6f7
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-07-18 13:18:40 +0700

    Update test.ts

--- src/server/test.ts
+++ src/server/test.ts
@@ -1,6 +1,8 @@
 import test from "ava";
 
 import { IGame } from "../package/interface/IGame";
+import { IRoom } from "../package/interface/IRoom";
+
 import { GameImpl } from "../package/implementation/GameImpl";
 import { RoomImpl } from "../package/implementation/RoomImpl";
 
@@ -15,7 +17,7 @@ test.before((t) => {
 });
 
 test("Get Room", (t) => {
-    const room = game.getRoom(1);
+    const room: IRoom = game.getRoom(1);
     const expectedRoom = new RoomImpl(1);
 
     t.deepEqual(room, expectedRoom, "Room isn't same!");
