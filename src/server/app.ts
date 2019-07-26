03b3dadb1513b61c339327362eb6ff3a733419af
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-07-26 23:09:32 +0700

    Remove unecessary import

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,4 +1,4 @@
-import bodyParser, { urlencoded } from "body-parser";
+import bodyParser from "body-parser";
 import express from "express";
 import { registerRoute } from "../package/routes/list";
 
